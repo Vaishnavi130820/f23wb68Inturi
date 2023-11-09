0c6530353e7bca9bffcdaa0204b03ea9ecf42417
Vaishnavi130820 <143207526+Vaishnavi130820@users.noreply.github.com>
2023-11-08 23:26:41 -0600

    added the database

--- controllers/animals.js
+++ controllers/animals.js
@@ -33,3 +33,36 @@ exports.animals_list = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
+
+   // VIEWS
+// Handle a show all view
+exports.animals_view_all_Page = async function(req, res) {
+    try{
+    theanimals = await animals.find();
+    res.render('animals', { title: 'animals Search Results', results: theanimals });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
+   // Handle Costume create on POST.
+exports.costume_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new animals();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.animalName = req.body.animalName;
+    document.animalAge = req.body.animalAge;
+    document.animalCost = req.body.animalCost;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
--- models/animals.js
+++ models/animals.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose") 
 const animalsSchema = mongoose.Schema({ 
-    animalName: String,  	animalAge: String, 
- 	Cost: Number 
+    animalName: String,
+    animalAge: String, 
+    animalCost: Number 
 })  
 module.exports = mongoose.model("animals", animalsSchema) 
