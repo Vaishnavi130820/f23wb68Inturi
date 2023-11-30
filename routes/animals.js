d6e39ad1eb27bea7dc427dad3c74427830e2015a
Vaishnavi130820 <143207526+Vaishnavi130820@users.noreply.github.com>
2023-11-29 20:43:40 -0600

    updated data

--- routes/animals.js
+++ routes/animals.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var passport = require('passport');
 const animals_controlers= require('../controllers/animals');
 var router = express.Router();
 /* GET animals */
@@ -7,23 +8,23 @@ router.get('/', animals_controlers.animals_view_all_Page );
 router.get('/detail', animals_controlers.animals_view_one_Page);
 /* GET create animals page */
 router.get('/create', animals_controlers.animals_create_Page);
-/* GET create animals page */
-//router.get('/update', animals_controlers.animals_update_Page);
+/* GET create update page */
+
 // A little function to check if we have an authorized user and continue on
-or
+//or
 // redirect to login.
 const secured = (req, res, next) => {
- if (req.user){
- return next();
- }
- res.redirect("/login");
- }
- /* GET update costume page */
-router.get('/update', secured,
-animals_controlers.animals_update_Page);
-router.post('/login', passport.authenticate('local'), function(req, res) {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
+    /* GET update costume page */
+    router.get('/update', secured,
+    animals_controlers.animals_update_Page);
+    router.post('/login', passport.authenticate('local'), function(req, res) {
     res.redirect('/');
-   });
+    });
 /* GET delete animals page */
 router.get('/delete', animals_controlers.animals_delete_Page);
 
