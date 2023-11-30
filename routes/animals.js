f760fd8db646bd03f536f75b97e7ea5a77a8412e
Vaishnavi130820 <143207526+Vaishnavi130820@users.noreply.github.com>
2023-11-29 20:25:02 -0600

    added login

--- routes/animals.js
+++ routes/animals.js
@@ -8,7 +8,22 @@ router.get('/detail', animals_controlers.animals_view_one_Page);
 /* GET create animals page */
 router.get('/create', animals_controlers.animals_create_Page);
 /* GET create animals page */
-router.get('/update', animals_controlers.animals_update_Page);
+//router.get('/update', animals_controlers.animals_update_Page);
+// A little function to check if we have an authorized user and continue on
+or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ res.redirect("/login");
+ }
+ /* GET update costume page */
+router.get('/update', secured,
+animals_controlers.animals_update_Page);
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+   });
 /* GET delete animals page */
 router.get('/delete', animals_controlers.animals_delete_Page);
 
